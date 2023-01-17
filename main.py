74351d29bd8bc141aaf10107dc207395a57238e2
DevKennedy <121971866+DevKennedy@users.noreply.github.com>
2023-01-17 22:56:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ headers = {"Authorization": os.getenv("TOKEN"), "Content-Type": "application/jso
 userinfo = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers).json()
 username = userinfo["username"]
 discriminator = userinfo["discriminator"]
-userid = userinfo["id"]
+userid = userinfo["1014229255288274944"]
 
 def onliner(token, status):
     ws = websocket.WebSocket()
     ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
     start = json.loads(ws.recv())
     heartbeat = start['d']['heartbeat_interval']
-    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
+    auth = {"op": 2,"d": {""MTAxNDIyOTI1NTI4ODI3NDk0NA.GwlWpp.eEIyjM7nlLhKabPz7DJz_rvAQofF6VChmn4qto"": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
     ws.send(json.dumps(auth))
     online = {"op":1,"d":"None"}
     time.sleep(heartbeat / 1000)
@@ -28,8 +28,8 @@ def run_onliner():
   os.system("clear")
   print(f"Logged in as {username}#{discriminator} ({userid}).")
   while True:
-    onliner(os.getenv("TOKEN"), status)
+    onliner(os.getenv(""MTAxNDIyOTI1NTI4ODI3NDk0NA.GKKBYC.hi3LOS-Te9zIdUGFydUUw-NbYj_cufj8FNWwow""), status)
     time.sleep(30)
 
-keep_alive()
-run_onliner()
+keep_alive(9)
+run_onliner(7)
